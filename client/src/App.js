23d1284f6e367f14fbc5c5179bf19cbf4c03783a
septume <duchene.nico.som@gmail.com>
2022-12-20 09:52:04 +0100

    new final commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+import React, { Fragment } from "react";
+
+import InputTodo from "./components/InputTodo";
+import ListTodos from "./components/ListTodos";
+
+//components
+
+
+function App() {
+  return (
+  <Fragment>
+    <div className='container'>
+      <InputTodo />
+      <ListTodos/>
+    </div>
+  </Fragment>
+  );
+}
+
+export default App;
